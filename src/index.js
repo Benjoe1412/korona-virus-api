10123291bad203e419e5e0db7f4f6d699fd94f04
Benjoe1412 <benjoe1412@gmail.com>
2021-04-23 12:20:40 +0200

    module path changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
-const data = require('./data')
-const timeSeries = require('./time-series')
+const data = require('./data.js')
+const timeSeries = require('./time-series.js')
 const express = require('express')
 const cors = require('cors')
 
